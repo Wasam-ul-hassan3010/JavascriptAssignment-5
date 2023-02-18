ddd5996d732c5adcb966e7a30bae672ef64d7e79
Wasam-ul-hassan3010 <124611992+Wasam-ul-hassan3010@users.noreply.github.com>
2023-02-18 20:06:28 +0500

    Add files via upload

---
+++ Assignment5/app.js
@@ -0,0 +1,93 @@
+// Question 01*
+// var studentsName = []
+// console.log(studentsName)
+
+// Question 02*
+// var studentsName = new Array()
+// console.log(studentsName)
+
+// Question 03*
+// var students = ["Wasam","Hassan","Amir"];
+// console.log(students)
+
+
+// Question 04*
+// var number = [1 , 2 , 3]
+// console.log(number)
+
+// Question 05*
+// var boolean = [true , false]
+// console.log(boolean)
+
+// Question 06*
+// var mixedArray = ["Wasam" , 20 , true]
+// console.log(mixedArray)
+
+// Question 07*
+// var educationInPakistan = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+// document.write("<h1> Qualifications </h1>")
+// document.write(educationInPakistan[0] + "<br />")
+// document.write(educationInPakistan[1] + "<br />")
+// document.write(educationInPakistan[2] + "<br />")
+// document.write(educationInPakistan[3] + "<br />")
+// document.write(educationInPakistan[4] + "<br />")
+// document.write(educationInPakistan[5] + "<br />")
+// document.write(educationInPakistan[6] + "<br />")
+// document.write(educationInPakistan[7] + "<br />")
+
+
+// Question 08*
+// var studentsName = ["Wasam" , "Amir" , "A.Rehman"]
+// var score = [400 , 450 , 350]
+// var totalMarks = 500
+// document.write("score of " + studentsName[0] + " is " + score[0] + " Percentage is: " + score[0] / totalMarks*100 + "<br />")
+// document.write("score of " + studentsName[1] + " is " + score[1] + " Percentage is: " + score[1] / totalMarks*100 + "<br />")
+// document.write("score of " + studentsName[2] + " is " + score[2] + " Percentage is: " + score[2] / totalMarks*100 + "<br />")
+
+
+// Question 09*
+// A.
+// var colorsNames = ["red" , "blue" , "green"]
+// document.write(colorsNames + "</br >")
+// var addColors = prompt("Enter any color you want to add to the begining")
+// colorsNames.unshift(addColors)
+// document.write(colorsNames)
+
+// B.
+// var colorsNames = ["red" , "blue" , "green"]
+// document.write(colorsNames + "</br >")
+// var addColors = prompt("Enter any color you want to add to the begining")
+// colorsNames.push(addColors)
+// document.write(colorsNames)
+
+// C.
+// var colorsNames = ["red" , "blue" , "green"]
+// document.write(colorsNames + "</br >")
+// var addColors = prompt("Enter any color you want to add to the begining")
+// colorsNames.unshift(addColors)
+// document.write(colorsNames)
+
+// D.
+// var colors = ["red" , "green" , "blue"]
+// colors.shift(colors)
+// document.write(colors)
+
+// E.
+// var colors = ["red" , "green" , "blue"]
+// colors.pop(colors)
+// document.write(colors)
+
+// F.
+// var colors = ["red" , "green" , "blue"]
+// var addColor = prompt("Enter any color anywhere you want to add")
+// var colorName = prompt("Give the color name")
+// console.log(colors)
+// colors.splice(addColor,0,colorName)
+// document.write(colors)
+
+// G.
+// var colors = ["red" , "green" , "blue"]
+// var dltColor = prompt("dlt the color at any index ")
+// var colorName = prompt("how many color you have deleted?")
+// colors.splice(addColor,0,colorName)
+// document.write(colors)
